b44f913cb56dcb6efc63812bd4b7794801189716
bogdankostic <bogdankostic@web.de>
2026-03-19 16:46:31 +0100

    Update test file

--- tests/test_example.py
+++ tests/test_example.py
@@ -2,35 +2,35 @@
 #
 # SPDX-License-Identifier: Apache-2.0
 
-# TODO: Add tests for your component(s). Below is a skeleton to get you started.
-#
-# import pytest
-#
-# from haystack_integrations.components.example import MyComponent
-#
-#
-# class TestMyComponent:
-#     def test_init(self):
-#         component = MyComponent()
-#         assert component is not None
-#
-#     def test_run(self):
-#         component = MyComponent()
-#         result = component.run(...)
-#         assert "output_name" in result
-#
-#     def test_to_dict(self):
-#         component = MyComponent()
-#         data = component.to_dict()
-#         assert data is not None
-#
-#     def test_from_dict(self):
-#         component = MyComponent()
-#         data = component.to_dict()
-#         deserialized = MyComponent.from_dict(data)
-#         assert deserialized is not None
-#
-#     @pytest.mark.integration
-#     def test_run_with_real_service(self):
-#         """Integration tests that require external services or API keys."""
-#         ...
+# TODO: Replace these example tests with tests for your own component(s).
+
+from haystack_integrations.components.example import ExampleComponent
+
+
+class TestExampleComponent:
+    def test_init_default(self):
+        component = ExampleComponent()
+        assert component.param == "default"
+
+    def test_init_custom_param(self):
+        component = ExampleComponent(param="custom")
+        assert component.param == "custom"
+
+    def test_run(self):
+        component = ExampleComponent()
+        result = component.run(input_text="Hello, world!")
+        assert result == {"output": "Hello, world!"}
+
+    def test_to_dict(self):
+        component = ExampleComponent(param="custom")
+        data = component.to_dict()
+        assert data == {
+            "type": "haystack_integrations.components.example.example_component.ExampleComponent",
+            "init_parameters": {"param": "custom"},
+        }
+
+    def test_from_dict(self):
+        component = ExampleComponent(param="custom")
+        data = component.to_dict()
+        deserialized = ExampleComponent.from_dict(data)
+        assert deserialized.param == "custom"
